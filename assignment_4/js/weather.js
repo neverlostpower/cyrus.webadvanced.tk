ab92a02bde6df27f601b7664c9e8c4bbb6a3e8c7
Cyrus Ra <rac459@newschool.edu>
2016-12-09 23:49:49 +0000

    message

--- assignment_4/js/weather.js
+++ assignment_4/js/weather.js
@@ -1,4 +1,22 @@
+var windSpeed;
+var windDirection;
+
 function getWeather() {
-    var location = "10013";
+    var location = "new york, ny";
 
     $.get('https://query.yahooapis.com/v1/public/yql?q=select * from weather.forecast where woeid in (select woeid from geo.places(1) where text="' + location + '")&format=json', function (data) {
+        /* Check that a place was found (we'll just grab the first) */
+        if (data.query.results === null) {
+            console.log("Location not found: " + location + "!");
+        } else {
+            console.log(data.query.results);
+            console.log(data.query.results.channel.wind.speed);
+            console.log(data.query.results.channel.wind.direction);
+            windSpeed = parseInt(data.query.results.channel.wind.speed);
+            windDirection = parseInt(data.query.results.channel.wind.direction);
+        }
+    });
+
+};
+
+getWeather();
